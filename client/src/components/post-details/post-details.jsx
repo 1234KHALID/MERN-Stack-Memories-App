f7adf5d071a4f9532604fb43feef82006920475c
Mohd Khalid <mohammad@prodios.com>
2025-05-07 21:04:15 +0530

    add comment section component

--- client/src/components/post-details/post-details.jsx
+++ client/src/components/post-details/post-details.jsx
@@ -5,6 +5,7 @@ import moment from 'moment';
 import { useParams, useNavigate } from 'react-router-dom';
 import useStyles from './styles';
 import { getPostById, getPostsBySearch } from '../../actions/posts';
+import CommentSection from './comment-section';
 
 const PostDetails = () => {
   const { post, posts, isLoading } = useSelector((state) => state.posts);
@@ -54,9 +55,7 @@ const PostDetails = () => {
             <strong>Realtime Chat - coming soon!</strong>
           </Typography>
           <Divider style={{ margin: '20px 0' }} />
-          <Typography variant="body1">
-            <strong>Comments - coming soon!</strong>
-          </Typography>
+          <CommentSection post={post} />
           <Divider style={{ margin: '20px 0' }} />
         </div>
         <div className={classes.imageSection}>
